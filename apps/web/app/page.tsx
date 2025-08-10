db08c5300fa8c141dd65c2d5608ca2189be1b277
gayetech <sheriffgaye5@gmail.com>
2025-08-10 10:33:55 +0000

    all

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,19 @@
-import { Button } from "@workspace/ui/components/button"
-import {add} from "@workspace/math/add"
+"use client";
+
+import { useQuery } from "convex/react"
+import {api} from "@workspace/backend/_generated/api"
 
 export default function Page() {
+
+  const users=useQuery(api.user.getMany)
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
         {
-          add(3,6)
+          JSON.stringify(users)
         }
+       
       </div>
     </div>
   )
